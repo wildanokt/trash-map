80f977f79b990372717dd31f958477ca9f0b3a76
Wildanokt <oktavianwww@gmail.com>
2019-09-22 14:08:47 +0700

    Initial commit

---
+++ app/src/main/java/com/wildanokt/trashmap/MainActivity.java
@@ -0,0 +1,58 @@
+package com.wildanokt.trashmap;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.wildanokt.trashmap.fragment.HelpFragment;
+import com.wildanokt.trashmap.fragment.HomeFragment;
+
+public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener{
+
+    private BottomNavigationView navigation;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        navigation = findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(this);
+        loadFragment(new HomeFragment());
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        Fragment fragment = null;
+        switch (menuItem.getItemId()){
+            case R.id.navigation_map:
+                fragment = new HomeFragment();
+                break;
+            case R.id.navigation_help:
+                fragment = new HelpFragment();
+                break;
+        }
+        return loadFragment(fragment);
+    }
+
+    @Override
+    public void onBackPressed() {
+        finish();
+    }
+
+    private boolean loadFragment(Fragment fragment) {
+        if (fragment != null) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.fragment_container, fragment)
+                    .commit();
+            return true;
+        }
+        return false;
+    }
+
+
+}
---
+++ app/src/main/java/com/wildanokt/trashmap/fragment/HomeFragment.java
@@ -0,0 +1,87 @@
+package com.wildanokt.trashmap.fragment;
+
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapsInitializer;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.wildanokt.trashmap.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class HomeFragment extends Fragment {
+
+
+    public HomeFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View rootView = inflater.inflate(R.layout.fragment_home, container, false);
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);  //use SuppoprtMapFragment for using in fragment instead of activity  MapFragment = activity   SupportMapFragment = fragment
+        mapFragment.getMapAsync(new OnMapReadyCallback() {
+            @Override
+            public void onMapReady(GoogleMap mMap) {
+                mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+
+                mMap.clear(); //clear old markers
+
+                CameraPosition googlePlex = CameraPosition.builder()
+                        .target(new LatLng(-7.952383, 112.614029))
+                        .zoom(16)
+                        .bearing(0)
+                        .tilt(30)
+                        .build();
+
+                mMap.animateCamera(CameraUpdateFactory.newCameraPosition(googlePlex), 3500, null);
+
+                mMap.addMarker(new MarkerOptions()
+                        .position(new LatLng(-7.953341, 112.614669))
+                        .title("FILKOM")
+                        .snippet("kapasitas terisi : 50%")
+                );
+
+
+                mMap.addMarker(new MarkerOptions()
+                        .position(new LatLng(-7.951762, 112.616007))
+                        .title("Masjid Raden Patah")
+                        .snippet("Kapasitas terisi : 40%")
+                );
+
+                mMap.addMarker(new MarkerOptions()
+                        .position(new LatLng(-7.950158, 112.613380))
+                        .title("Fak. Teknik")
+                        .snippet("kapasitas terisi : 80%")
+                );
+
+                mMap.addMarker(new MarkerOptions()
+                        .position(new LatLng(-7.952682, 112.611325))
+                        .title("Fak. Perikanan dan kelautan")
+                        .snippet("kapasitas terisi : 20%")
+                );
+            }
+        });
+
+        return  rootView;
+    }
+}
